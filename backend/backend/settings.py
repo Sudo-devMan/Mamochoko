68a9195d18d566758fd37be0f2557f280b7f60df
Sudo-devMan <inevitabledouble.e@gmail.com>
2025-12-19 15:22:19 +0200

    Updated CORS settings (added frontend url)

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -161,12 +161,12 @@ DEFAULT_FILE_STORAGE = 'staff_admin.storage.SupabaseStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
-CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWS_CREDENTIALS = True
-# CORS_ALLOWED_ORIGINS = [
-#     'http://localhost:5173',
-#     'https://mamochoko.vercel.app'
-# ]
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173',
+    'https://mamochoko.vercel.app'
+]
 
 
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
